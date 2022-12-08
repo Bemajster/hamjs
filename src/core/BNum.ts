069537a08bd5ba8cb0cc1b1f4e792c61fa7fdf43
Vajmir <sassiffy1004@gmail.com>
2022-12-08 17:17:36 +0100

    💥 0.0.2 Update
    Changes are described in /docs/changelog/0.0.2-version.md

---
+++ src/core/BNum.ts
@@ -0,0 +1,7 @@
+export class BNum {
+  val: string
+
+  constructor(p_num: number) {
+    this.val = p_num.toString()
+  }
+}
---
+++ test/core/BNum.test.ts
@@ -0,0 +1,5 @@
+describe("BNum tests", () => {
+  test("BNum tests - 1", () => {
+    expect(1).toBe(1)
+  })
+})
